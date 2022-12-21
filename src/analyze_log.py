dac97f251a4947fb86eac3a46290ff4de1819c2f
Kauã Maximino <maximinokaua@gmail.com>
2022-12-21 20:38:37 -0300

    feat: :sparkles: analyze_log func

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,12 @@
-def analyze_log(path_to_file):
-    raise NotImplementedError
+import csv
 
-# initial
+
+def analyze_log(file):
+    if '.csv' not in file:
+        raise FileNotFoundError(f"Extensão inválida: '{file}'")
+
+    try:
+        with open(file) as file_csv:
+            return csv.reader(file_csv)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{file}'")
