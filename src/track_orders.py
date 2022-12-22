6665286db4de6b4a7e9a81d9b3e7a697d1f87375
Kauã Maximino <maximinokaua@gmail.com>
2022-12-21 23:05:25 -0300

    feat: :sparkles: TrackOrders class

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,58 @@
+from collections import Counter
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.a = list()
+
     def __len__(self):
-        pass
+        return len(self.a)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.a.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        for custom, order, day in self.a:
+            if custom == customer:
+                orders = list()
+                orders.append(order)
+        return Counter(orders).most_common()[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        t_orders = set()
+
+        for custom, order, day in self.a:
+            t_orders.add(order)
+            if custom == customer:
+                l_orders = set()
+                l_orders.add(order)
+        return t_orders - l_orders
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        t_days = set()
+
+        for custom, order, day in self.a:
+            t_days.add(day)
+            if custom == customer:
+                days = set()
+                days.add(day)
+        return t_days - days
 
     def get_busiest_day(self):
-        pass
+        days = list()
+
+        for customer, order, day in self.a:
+            if customer == customer:
+                days.append(day)
+
+        maximum = max(days, key=days.count)        
+        return maximum
 
     def get_least_busy_day(self):
-        pass
+        for customer, order, day in self.a:
+            if customer == customer:
+                days = list()
+                days.append(day)
+        
+        minimum = min(days, key=days.count)
+        return minimum
